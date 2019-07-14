1985c755ca93bd8ede85e1f955815f573eec9bb5
Ivan <ivanspasic47@gmail.com>
2019-07-14 20:56:44 +0200

    Moved BLE functions and variables to separate module

---
+++ main/ble_if.h
@@ -0,0 +1,58 @@
+#include "esp_bt.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "ble_odometer.h"
+
+#ifndef BLE_IF_H
+#define BLE_IF_H
+
+#define CHAR_DECLARATION_SIZE       (sizeof(uint8_t))
+#define GATTS_TABLE_TAG             "GATTS_SPP_DEMO"
+#define SPP_PROFILE_NUM             1
+#define SPP_PROFILE_APP_IDX         0
+#define ESP_SPP_APP_ID              0x69
+#define SAMPLE_DEVICE_NAME          "BIKEMETER"
+#define SPP_SVC_INST_ID	            0
+/// Characteristic UUID
+#define ESP_GATT_UUID_SPP_DATA_RECEIVE      0xABF1
+#define ESP_GATT_UUID_SPP_DATA_NOTIFY       0xABF2
+
+
+extern uint16_t spp_handle_table[SPP_IDX_NB];
+extern uint16_t spp_conn_id;
+extern esp_gatt_if_t spp_gatts_if;
+
+/// SPP Service
+static const uint16_t spp_service_uuid = 0xABF0;
+
+/*
+ *  SPP PROFILE ATTRIBUTES
+ ****************************************************************************************
+ */
+
+static const uint16_t primary_service_uuid = ESP_GATT_UUID_PRI_SERVICE;
+static const uint16_t character_declaration_uuid = ESP_GATT_UUID_CHAR_DECLARE;
+static const uint16_t character_client_config_uuid = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
+
+static const uint8_t char_prop_read_notify = ESP_GATT_CHAR_PROP_BIT_READ|ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+static const uint8_t char_prop_read_write = ESP_GATT_CHAR_PROP_BIT_WRITE_NR|ESP_GATT_CHAR_PROP_BIT_READ;
+
+///SPP Service - data receive characteristic, read&write without response
+static const uint16_t spp_data_receive_uuid = ESP_GATT_UUID_SPP_DATA_RECEIVE;
+static const uint8_t  spp_data_receive_val[20] = {0x00};
+
+///SPP Service - data notify characteristic, notify&read
+static const uint16_t spp_data_notify_uuid = ESP_GATT_UUID_SPP_DATA_NOTIFY;
+static const uint8_t  spp_data_notify_val[20] = {0x00};
+static const uint8_t  spp_data_notify_ccc[2] = {0x00, 0x00};
+
+
+uint8_t find_char_and_desr_index(uint16_t handle);
+bool store_wr_buffer(esp_ble_gatts_cb_param_t *p_data);
+void free_write_buffer(void);
+void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+#endif
--- main/ble_odometer.h
+++ main/ble_odometer.h
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef BLE_BIKEMETER_H
+#define BLE_BIKEMETER_H
 /*
  * DEFINES
  ****************************************************************************************
@@ -23,6 +25,7 @@
 #define SPP_CMD_MAX_LEN            (20)
 #define SPP_STATUS_MAX_LEN         (20)
 #define SPP_DATA_BUFF_MAX_LEN      (2*1024)
+
 ///Attributes State Machine
 enum{
     SPP_IDX_SVC,
@@ -34,12 +37,7 @@ enum{
     SPP_IDX_SPP_DATA_NTY_VAL,
     SPP_IDX_SPP_DATA_NTF_CFG,
 
-    SPP_IDX_SPP_COMMAND_CHAR,
-    SPP_IDX_SPP_COMMAND_VAL,
-
-    SPP_IDX_SPP_STATUS_CHAR,
-    SPP_IDX_SPP_STATUS_VAL,
-    SPP_IDX_SPP_STATUS_CFG,
-
     SPP_IDX_NB,
 };
+
+#endif
